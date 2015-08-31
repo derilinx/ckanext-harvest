2be89257bc17764541f3d23430a4864650d7bb08
David Read <david.read@hackneyworkshop.com>
2015-08-31 18:07:40 +0100

    [1895] Fix recreation of resources every harvest.

--- ckanext/harvest/tests/test_base.py
+++ ckanext/harvest/tests/test_base.py
@@ -28,6 +28,27 @@ class TestMungeTags:
 
 
 def test_extras_from_dict():
-    res = HarvesterBase.extras_from_dict({'theme': 'environment', 'freq': 'daily'})
+    res = HarvesterBase.extras_from_dict({'theme': 'environment',
+                                          'freq': 'daily'})
     assert_equal(res, [{'key': 'theme', 'value': 'environment'},
                        {'key': 'freq', 'value': 'daily'}])
+
+
+def test_match_resources_with_existing_ones():
+    res_dicts = [{'url': 'url1', 'name': 'name', 'description': 'desc'},
+                 {'url': 'url3', 'name': 'name', 'description': 'desc'},
+                 {'url': 'url2', 'name': 'name', 'description': 'desc'},
+                 {'url': 'url4', 'name': 'name', 'description': 'desc'},
+                 ]
+    existing_resources = [
+        {'id': '1', 'url': 'url1', 'name': 'name', 'description': 'desc'},
+        {'id': 'BAD', 'url': 'BAD', 'name': 'name', 'description': 'desc'},
+        {'id': '2', 'url': 'url2', 'name': 'BAD', 'description': 'desc'},
+        {'id': '4', 'url': 'url4', 'name': 'BAD', 'description': 'desc'},
+        ]
+    HarvesterBase._match_resources_with_existing_ones(res_dicts,
+                                                      existing_resources)
+    assert_equal(res_dicts[0].get('id'), '1')
+    assert_equal(res_dicts[1].get('id'), None)
+    assert_equal(res_dicts[2].get('id'), '2')
+    assert_equal(res_dicts[3].get('id'), '4')
