12e1a6664b6224ce3a011325a45f74c86a0221b5
Carl Lange <carl@flax.ie>
2017-12-04 17:17:14 +0000

    add a session.close() to stop postgres holding many halfopen conns

--- ckanext/harvest/logic/action/update.py
+++ ckanext/harvest/logic/action/update.py
@@ -684,6 +684,8 @@ def harvest_jobs_run(context, data_dict):
     # Resubmit pending objects missing from Redis
     resubmit_objects()
 
+    session.close()
+
     return []  # merely for backwards compatibility
 
 
