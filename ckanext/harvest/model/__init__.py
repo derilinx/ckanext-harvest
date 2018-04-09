8efaa11b9fc9b3ab9223ab8e8258a10e2f39fc2e
Eric Soroos <eric@derilinx.com>
2018-04-09 11:22:49 +0100

    Slightly more informative error message for logging

--- ckanext/harvest/model/__init__.py
+++ ckanext/harvest/model/__init__.py
@@ -251,7 +251,8 @@ class HarvestObjectError(HarvestDomainObject):
         finally:
             log_message = '{0}, line {1}'.format(message, line) \
                           if line else message
-            log.debug(log_message)
+            log.debug('HarvestObjectError: %s' %log_message)
+
 
 
 class HarvestLog(HarvestDomainObject):
