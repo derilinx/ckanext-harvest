f4bc2471be9337de9c039640e474a317469d4f96
Eric Soroos <eric@derilinx.com>
2018-04-17 15:10:23 +0100

    Force all the modifications to harvesters to be sysadmin only

--- ckanext/harvest/logic/auth/delete.py
+++ ckanext/harvest/logic/auth/delete.py
@@ -5,23 +5,11 @@ def harvest_source_delete(context, data_dict):
     '''
         Authorization check for harvest source deletion
 
-        It forwards the checks to package_delete, which will check for
-        organization membership, whether if sysadmin, etc according to the
-        instance configuration.
+
+        sysadmins only  *dgi3
     '''
-    model = context.get('model')
-    user = context.get('user')
-    source_id = data_dict['id']
 
-    pkg = model.Package.get(source_id)
-    if not pkg:
-        raise pt.ObjectNotFound(pt._('Harvest source not found'))
-
-    context['package'] = pkg
-
-    try:
-        pt.check_access('package_delete', context, data_dict)
+    if not user_is_sysadmin(context):
+        return {'success': False, 'msg': pt._('Only sysadmins can delete harvest sources')}
+    else:
         return {'success': True}
-    except pt.NotAuthorized:
-        return {'success': False,
-                'msg': pt._('User {0} not authorized to delete harvest source {1}').format(user, source_id)}
